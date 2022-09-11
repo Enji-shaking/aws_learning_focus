66321e61820dbf5ae47623692851324121300e2e
Enji Li Mac <enjili@usc.edu>
2022-09-11 15:25:05 -0700

    working

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "learningfocus956ef204": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "api": {
+        "learningfocus": {
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+
+import {withAuthenticator} from '@aws-amplify/ui-react'
+import {API} from 'aws-amplify'
+import { useEffect, useState } from 'react'
+import {listPets} from './graphql/queries'
+import {createPet, deletePet} from './graphql/mutations'
 
 function App() {
+  const [petsData, setPetsData] = useState([])
+  useEffect( () => {
+    const fetchObj = async () => {
+      const res = await API.graphql({query: listPets})
+      return res.data.listPets.items
+    }
+    fetchObj().then(pets=>setPetsData(pets))
+  }, []) 
+
+  const handleSubmit = async (event) => {
+    
+    event.preventDefault()
+    try{
+      const {data} = await API.graphql({
+        query: createPet,
+        variables: {
+          input: {
+            name: event.target.petName.value,
+            description: event.target.petDescription.value,
+            petType: event.target.petType.value
+          },
+        }
+      })
+      setPetsData([...petsData, data.createPet])
+    }catch(error){
+      console.log(error)
+    } 
+  }
+
+  const handlePetDelete = async (petID) => {
+    const newPetList = petsData.filter(pet => pet.id !== petID)
+    try{
+      await API.graphql({
+        query: deletePet,
+        variables: {
+          input: {
+            id: petID
+          }
+        }
+      })
+      setPetsData(newPetList)
+    }catch(error){
+      console.log(error)
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input placeholder='enter a name' name = 'petName' />
+        <input placeholder='enter a description' name = 'petDescription' />
+        <select name = 'petType'>
+          <option value='CAT'>Cat</option>
+          <option value='BIRD'>Bird</option>
+          <option value='DOG'>Dog</option>
+          <option value='OTHER'>Other</option>
+        </select>
+        <button>Create pet</button>
+      </form>
+      <main>
+        <ul>
+          {
+            petsData.map((pet) => (
+              <li onClick = {() => handlePetDelete(pet.id)}>
+                <article>
+                  <h2>{pet.name}</h2>
+                  <h5>{pet.type}</h5>
+                  <p>{pet.description}</p>
+                </article>
+              </li>
+            ))
+          }
+        </ul>
+      </main>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
